17f73ea8d0c01ca5de2cc117d917c63185276dc1
Admin <Admin@DESKTOP-0UCIBC8>
2024-03-14 17:13:14 +0530

    Initial Commit.

---
+++ Seema18/src/main/java/com/Seema/OrangeHRM.java
@@ -0,0 +1,41 @@
+package com.Seema;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class OrangeHRM {
+
+	public static void main(String[] args) {
+System.setProperty("Webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver_win32(1)\\chromedriver.exe");
+		
+        ChromeOptions options = new ChromeOptions();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		WebElement usernameInput = driver.findElement(By.id("txtUsername"));
+		WebElement passwordInput = driver.findElement(By.id("txtPassword"));
+		WebElement loginButton = driver.findElement(By.id("btnLogin"));
+		
+		
+		
+		
+		usernameInput.sendKeys("Admin");
+		passwordInput.sendKeys("admin123");
+		
+		
+		loginButton.click();
+		driver.quit();
+		
+		
+	
+
+	}
+
+
+	}
+
+
---
+++ Seema18/src/main/java/com/Seema/OrangeHRMTest.java
@@ -0,0 +1,10 @@
+package com.Seema;
+
+public class OrangeHRMTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
